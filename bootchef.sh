fd8959882af8dcbe0817def827dab4e40056c3da
DakshEada <abhi.12189@gmail.com>
2017-07-26 22:44:45 -0400

    Update bootchef.sh
    new bootchef config file

--- bootchef.sh
+++ bootchef.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-
-sed -i s#Defaults.*requiretty#Defaults\ \!requiretty#g /etc/sudoers
+sudo sed -i s#Defaults.*requiretty#Defaults\ \!requiretty#g /etc/sudoers
+sudo yum -y makecache fast
+sudo yum -y install wget
 cd /tmp/
 echo "Downloading the rpm"
-wget  http://52.43.11.64/chef-12.19.36-1.el7.x86_64.rpm
+wget  http://52.43.11.64/chef-13.2.20-1.el6.x86_64.rpm
 
 echo "Installating the rpm"
 
 sudo rpm -ivh *.rpm
 
 sudo mkdir -p /etc/chef
-
+sudo chmod -R 777 /etc/chef
 cd /etc/chef
 
-wget http://52.43.11.64//client.rb
-#wget http://54.202.25.127/knife.rb
-wget http://52.43.11.64//sandykarhty20.pem
-chmod 400 sandykarhty.pem
+sudo curl -o /etc/chef/client.rb  http://52.43.11.64//client.rb
+#wget http://52.43.11.64/knife.rb
+sudo curl -o /etc/chef/sandykarhty20.pem  http://52.43.11.64/sandykarhty20.pem
+sudo chmod 400 /etc/chef/sandykarhty20.pem
 sudo chef-client
