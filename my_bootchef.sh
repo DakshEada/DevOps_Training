24362c2f5c2029cc9e04c07a74fe3b11a6dc84dc
DakshEada <abhi.12189@gmail.com>
2017-08-10 15:40:52 -0400

    Update my_bootchef.sh

--- my_bootchef.sh
+++ my_bootchef.sh
@@ -4,7 +4,7 @@ sudo yum -y makecache fast
 sudo yum -y install wget
 cd /tmp/
 echo "Downloading the rpm"
-wget  http://13.58.45.200/chef-13.2.20-1.el7.x86_64.rpm
+wget  http://18.220.166.106/chef-13.2.20-1.el7.x86_64.rpm
 
 echo "Installating the rpm"
 
@@ -15,8 +15,8 @@ sudo chmod -R 777 /etc/chef
 cd /etc/chef
 echo "COMPONENT=::COMPONENT::">envdata
 echo "ENVIRONMENT=::ENVIRONMENT::">>envdata
-sudo curl -o /etc/chef/client.rb  http://13.58.45.200//client.rb
+sudo curl -o /etc/chef/client.rb  http://18.220.166.106//client.rb
 #wget http://52.43.11.64/knife.rb
-sudo curl -o /etc/chef/daksh_eada.pem  http://13.58.45.200/daksh_eada.pem
+sudo curl -o /etc/chef/daksh_eada.pem  http://18.220.166.106/daksh_eada.pem
 sudo chmod 400 /etc/chef/daksh_eada.pem
 sudo chef-client
